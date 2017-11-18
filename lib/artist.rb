dacafad116066db1949bb90a21d983d8541ffee8
Pamela Frieze <pfrieze@gmail.com>
2017-11-18 22:21:31 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name
+end
+
+class Author
+  attr_accessor :name
+end
+
+class Post
+  attr_accessor :title, :author
+
+  def initialize
+    @title = title
+    @author = author
+  end
+end
+
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+    @artist = artist
+  end
+end
